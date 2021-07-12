cddd205ab9026badf2b7fda0c44db8f300c050bd
arrang3710 <87233765+arrang3710@users.noreply.github.com>
2021-07-12 20:59:16 +0800

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,24 @@
+http = require("http");
+
+http.createServer(function (request, response) {
+
+// Send the HTTP header
+
+// HTTP Status: 200 : OK
+
+// Content Type: text/plain
+
+response.writeHead(200, {'Content-Type': 'text/plain'});
+
+// Send the response body as "Hello World"
+
+response.end('Hello World');
+
+console.log(request.url)
+
+}).listen(4001);
+
+// Console will print the message
+
+console.log('Server running at http://129.213.54.196:4001/');
+© 2021 GitHub, Inc.
